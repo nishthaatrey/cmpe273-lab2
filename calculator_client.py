8127720c9cd22317c8b02766fc0ef56969385f66
Nishtha Atrey <33688691+nishthaatrey@users.noreply.github.com>
2018-09-08 18:12:51 -0700

    Update calculator_client.py

--- calculator_client.py
+++ calculator_client.py
@@ -8,10 +8,13 @@ import calculator_pb2_grpc
 
 def run():
     channel = grpc.insecure_channel('localhost:50051')
-    stub = calculator_pb2_grpc.CalculatorStub(channel)
+    stub = calculator_pb2_grpc.CalculatorServiceStub(channel)
    
-    response = stub.add(calculator_pb2.CalculatorRequest(a=1,b=2))
-    print("Sum is: " + response.message)
+    addNum = calculator_pb2.CalculatorRequest(num1=1,num2=5)
+    response = stub.Add(addNum)
+    print("Sum is: " + response.sum)
 
 if __name__ == '__main__':
     run()
+
+

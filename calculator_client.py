42d670d78e77885980d4a21ac342c17a244a1d90
Nishtha Atrey <33688691+nishthaatrey@users.noreply.github.com>
2018-09-08 22:57:59 -0700

    Update calculator_client.py

--- calculator_client.py
+++ calculator_client.py
@@ -12,9 +12,7 @@ def run():
    
     addNum = calculator_pb2.CalculatorRequest(num1=1,num2=5)
     response = stub.Add(addNum)
-    print("Sum is: " + response.sum)
+    print("Sum is: " + str(response.sum))
 
 if __name__ == '__main__':
     run()
-
-
